0707b187db58b3d82928f0fd48dca40480f99475
markbennett1973 <mbennett@rawnet.com>
2018-01-21 11:46:29 +0000

    Updated social links

--- application/themes/sparrowtail/elements/header.php
+++ application/themes/sparrowtail/elements/header.php
@@ -6,9 +6,9 @@ $this->inc('elements/header_top.php');
 
 <div id="pre-header" class="clearfix">
     <ul id="header-social" class="clearfix">
-        <li><a target="_blank" title="Sparrowtail in Facebook" href="https://www.facebook.com/mark.s.bennett"><img alt="Facebook" src="<?php echo $view->getThemePath()?>/images/social/facebook.png"> </a></li>
         <li><a target="_blank" title="Sparrowtail in Twitter" href="http://www.twitter.com/mark_bennett"><img alt="Twitter" src="<?php echo $view->getThemePath()?>/images/social/twitter.png"> </a></li>
-        <li><a target="_blank" title="Sparrowtail in Google+" href="https://plus.google.com/u/0/115048066798492404654/about"><img alt="Google+" src="<?php echo $view->getThemePath()?>/images/social/google.png"> </a></li>
+        <li><a target="_blank" title="Sparrowtail in LinkedIn+" href="https://www.linkedin.com/in/mark-bennett/"><img alt="LinkedIn+" src="<?php echo $view->getThemePath()?>/images/social/linkedin.png"> </a></li>
+        <li><a target="_blank" title="Sparrowtail in Github" href="https://github.com/markbennett1973""><img alt="Github" src="<?php echo $view->getThemePath()?>/images/social/github.png"> </a></li>
         <li><a target="_blank" title="Sparrowtail in RSS" href="/rss.xml"><img alt="RSS" src="<?php echo $view->getThemePath()?>/images/social/rss.png"> </a></li>
     </ul>
 </div>
